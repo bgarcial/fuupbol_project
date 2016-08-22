d7768a3fcbefe6bcce34db08ff03e5273a761511
Bernardo Garcia <botibagl@gmail.com>
2016-08-22 11:25:44 -0500

    test

--- fuupbol/settings/production.py
+++ fuupbol/settings/production.py
@@ -4,6 +4,17 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': 'ec2-54-235-221-102.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
 # ------ *** -------------
 # For deploy to heroku
 # ------ *** -------------
