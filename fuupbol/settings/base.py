bc5180cb326d3d2897bfb2f18bcf304e109babec
Bernardo Garcia <botibagl@gmail.com>
2016-10-29 20:52:37 -0500

    settings and admin

--- fuupbol/settings/base.py
+++ fuupbol/settings/base.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third party apps
-    'checkboxselectmultiple',
+    #'checkboxselectmultiple',
     'rest_framework',
     'storages',
     'multiselectfield',
