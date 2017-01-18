9299efc928339338c5bd97d460aacccfafb41a99
Bernardo Garcia <botibagl@gmail.com>
2017-01-18 18:14:55 -0500

    encrypt password user when is created in DRF

--- userprofiles/serializers.py
+++ userprofiles/serializers.py
@@ -69,13 +69,43 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'username','password','first_name','last_name','age','sex',
-        'photo','email','is_player','team','position','is_staff','is_active','is_superuser',
-        'is_player','weight','height','nickname','number_matches','accomplished_matches',
-        'time_available','leg_profile','number_shirt_preferred','team_support',
-        'player_preferred','last_login',)
+        fields = ('url', 'username', 'password', 'first_name','last_name',
+                  'age', 'sex', 'photo', 'email', 'is_player', 'team',
+                  'position', 'is_staff', 'is_active', 'is_superuser',
+                  'is_player', 'weight', 'height', 'nickname',
+                  'number_matches', 'accomplished_matches',
+                  'time_available', 'leg_profile', 'number_shirt_preferred',
+                  'team_support', 'player_preferred', 'last_login',)
 
-        #depth=1
+        # depth=1
 
-
-
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            age=validated_data['age'],
+            sex=validated_data['sex'],
+            photo=validated_data['photo'],
+            is_player=validated_data['is_player'],
+            team=validated_data['team'],
+            position=validated_data['position'],
+            is_staff=validated_data['is_staff'],
+            is_active=validated_data['is_active'],
+            is_superuser=validated_data['is_superuser'],
+            weight=validated_data['weight'],
+            height=validated_data['height'],
+            nickname=validated_data['nickname'],
+            number_matches=validated_data['number_matches'],
+            accomplished_matches=validated_data['accomplished_matches'],
+            time_available=validated_data['time_available'],
+            leg_profile=validated_data['leg_profile'],
+            number_shirt_preferred=validated_data['number_shirt_preferred'],
+            team_support=validated_data['team_support'],
+            player_preferred=validated_data['player_preferred'],
+            last_login=validated_data['last_login'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
