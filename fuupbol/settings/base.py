43fcde0cf8b664da0a241ecc82cdd26a8007e938
Bernardo Garcia <botibagl@gmail.com>
2017-01-20 09:58:38 -0500

    adding DEFAULT_AUTHENTICATION_CLASSES to DRF

--- fuupbol/settings/base.py
+++ fuupbol/settings/base.py
@@ -66,12 +66,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
-    '''
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    '''
 
     'PAGE_SIZE': 10
 }
